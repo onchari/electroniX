b60a3a3f336466cafdc4fb31b29dffa3eb4ddcda
onchari <onchariwycliffe@gmail.com>
2018-05-06 21:19:28 +0300

    Add project files.

---
+++ electroniX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(electroniX.Startup))]
+namespace electroniX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
